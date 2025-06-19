12330536ef16949b7b8c8e881114ba4150c4e7b0
Arjun <61193215+ArjunR00T@users.noreply.github.com>
2025-06-19 09:54:05 +0530

    Update TestCaseGenerator.tsx

--- src/components/TestCaseGenerator.tsx
+++ src/components/TestCaseGenerator.tsx
@@ -27,7 +27,7 @@ const TestCaseGenerator: React.FC = () => {
   const [hasGenerated, setHasGenerated] = useState(false);
   const [activeTab, setActiveTab] = useState("testCases");
 
-  const BaseUrl = "https://explain-watt-starring-copying.trycloudflare.com";
+  const BaseUrl = "https://closes-praise-sitemap-utc.trycloudflare.com";
   const generateTestCases = async () => {
     // 1. Initial Loading State for the whole process
     setIsLoading(true);
